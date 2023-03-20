234e4e0db49984fe36d120a6b020699bf8e86076
Wagner Duarte <wagnerofduarte@gmail.com>
2023-03-20 14:31:32 -0300

    Initial Commit

---
+++ Alura-Viagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alura-Viagens
+//
+//  Created by Wagner Duarte on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
