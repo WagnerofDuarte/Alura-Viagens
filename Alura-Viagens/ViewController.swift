9c51866ae99614085708478ca63dd50f80dd76cc
Wagner Duarte <wagnerofduarte@gmail.com>
2023-03-21 13:31:14 -0300

    Curso iOS - auto layout com Constraints - Parte 1

--- Alura-Viagens/ViewController.swift
+++ Alura-Viagens/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    //MARK: - OUTLETS
+    
+    @IBOutlet weak var viagensTableView: UITableView!
+    
+    //MARK: - INITS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viagensTableView.dataSource = self
     }
+    
+    //MARK: - TABLEVIEW PROTOCOLS
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "viagem \(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
